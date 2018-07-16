9c60751172ffafe02ff458821a5a180c19c4c88a
wkreiser <williamkreiser@outlook.com>
2018-07-16 14:57:37 -0500

    Capstone.c with binary conversion

--- Ch6labs/Capstone.c
+++ Ch6labs/Capstone.c
@@ -1,5 +1,5 @@
 /*  Name: William Kreiser
-    Date: 13 July 18
+    Date: 16 July 18
     Project: Capstone 
 */
 // 1: Flip a single bit using XOR
@@ -7,43 +7,43 @@
 #include <stdio.h>
 #include <inttypes.h>
 
-int binary(unsigned int num);
+int binary(uint32_t num);
 
 int main (void)
 {   //Initialize an unsigned int with an arbitrary value
-    unsigned int initialValue = 100, tempValue, newValue;
-    unsigned int inputMask = 0, tempMask = 0, binMask;
-    
-    //Ask the user input on which bit position to flip (0 through ((sizeof(unsigned int)*8) -1))
-    printf("Please select a number between 0-31: ");
-    scanf("%2u", &inputMask);
+    uint32_t initialValue = 13689; 
+    uint32_t tempValue = 0;
+    uint32_t newValue = 0;
 
-    //Converting the initial Value to binary 
-    tempValue = binary(initialValue);
-    printf("Binary Equivalent of Decimal Integer %u\n", tempValue);
+    uint32_t inputMask = 0;
+    uint32_t tempMask = 0;
+    uint32_t binMask = 0;
+    uint32_t bitMask = 0x01;
+    
+    //Ask the user input on which bit position to flip (0 through   ((sizeof(unsigned int)*8) -1))
+    printf("Please select a number between 0-31: ");
+    scanf("%u", &inputMask);
+
+    //Converting the initial Value to binary
+    printf("%u in binary is: ", initialValue);
+    binary(initialValue);
     //Create a dynamic bit mask based on the user input "inputMask"
-    tempMask = 1U << inputMask;
-    binMask = binary(tempMask);    
-    printf("The tempMask is: %d\n", binMask);
+    tempMask |= bitMask << inputMask;    
+    printf("The tempMask is: ");
+    binary(tempMask);
     // XOR the original value with the inputMask and print the result
     newValue = initialValue ^ tempMask;
-    printf("The XOR'd value is: %d\n", binary(newValue));
+    printf("The XOR'd value is: %zu\n", newValue);
+    printf("The XOR'd value in binary is: \n");
+    binary(newValue);
 
     return 0;
 }
 
-int binary(unsigned int num)
+int binary (uint32_t num)
 {
-    int rem, a = 1;
-    unsigned int bin_num = 0;
-
-    while(num > 0)
-    {
-        rem = num%2;
-        num = num / 2;
-        bin_num = bin_num + (a * rem);
-        a = a * 10;
-    }
-
-return bin_num;
+    unsigned i;
+    for (i = 1 << 31; i > 0; i = i / 2) 
+       (num & i) ? printf("1") : printf("0");
+    printf("\n");
 }
